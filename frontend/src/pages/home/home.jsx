5f90861afa9d1caa9eaecb511279df685c00a779
Sandra Kisambo <143804116+Ilavoga@users.noreply.github.com>
2024-11-23 16:00:09 +0000

    alpha ii

---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home.css'
+import Header from '../../components/header/header';
+import ExploreMenu from '../../components/exploreMenu/exploreMenu';
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <ExploreMenu/>
+    </div>
+  );
+};
+
+export default Home;
